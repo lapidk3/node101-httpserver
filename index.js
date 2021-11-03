8c7698002e673853162a576950809e116ac5aaff
Kenneth David <lapidk3@yahoo.com>
2021-11-03 16:06:03 -0700

    Created my own server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// write your code here
+const http = require('http'); // Require http library to start the servger
+const PORT = 8080; // The port our server will listen to 
+
+const server =  http.createServer(function(req, res) {
+    // load a msg each time a request is received
+    console.log('Receieved' + req.method + ' request for: ' + req.url);
+
+    // send headers
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // send body response
+    res.end("Hello World");
+});
+
+
+server.listen(PORT, "localhost", null, function(){
+    console.log("Server is listening on localhost:8080");
+});
